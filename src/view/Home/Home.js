7c8533dca3d8bde70a7125e7e11689453fa58369
your-denys <99995293+your-denys@users.noreply.github.com>
2023-05-11 20:42:55 +0300

    Update Home.js

--- src/view/Home/Home.js
+++ src/view/Home/Home.js
@@ -2,7 +2,7 @@ import Header from '../../components/Header/Header';
 import Navigation from '../../components/Navigation/Navigation';
 import Container from 'react-bootstrap/Container';
 import HomeContent from '../../components/HomeContent/HomeContent';
-import { motion as m } from 'framer-motion';
+import { motion } from 'framer-motion';
 import './Home.css';
 
 
@@ -10,17 +10,23 @@ import './Home.css';
 const Home = () => {
 
   return (
-    <m.div
+    <motion.div
       transition={{ duration: 0.75, ease: 'easeInOut' }}
-      exit={{opacity:0}}
+      
       className="home-page"
     >
       <Container>
         <Header />
         <HomeContent />
         <Navigation />
+        <motion.div
+        initial={{ width: 0 }}
+        whileInView={{ width: '100%' }}
+        transition={{ duration: 0.7 }}
+        className="border-dashed"
+      ></motion.div>
       </Container>
-    </m.div>
+    </motion.div>
   );
 };
 
