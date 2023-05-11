1e95d07e56ad42b15439968d233a5b6afc76db6c
your-denys <99995293+your-denys@users.noreply.github.com>
2023-05-11 20:18:25 +0300

    Update ContactUsContent.js

--- src/components/ContactUsContent/ContactUsContent.js
+++ src/components/ContactUsContent/ContactUsContent.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import Form from './Form'
 import './ContactUsContent.css'
+import { motion } from 'framer-motion';
 
 const ContactUsContent = () => {
   return (
     <section className='contact-us'>
-        <h2 className='contact-us-title'>Contact us</h2>
+            <motion.h2
+        initial={{ y: 60, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ delay: 0.1, duration: 0.6 }}
+        className="contact-us-title"
+      >
+        Contact us
+      </motion.h2>
+      <motion.div
+        initial={{ width: 0 }}
+        whileInView={{ width: '100%' }}
+        transition={{ duration: 0.7 }}
+        className="border-dashed"
+        style={{ marginBottom: '100px' }}
+      ></motion.div>
         <Form/>
     </section>
   )
